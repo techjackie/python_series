c28d61eb677bb427ea72d95571210781e2af6d5e
clearcuttech <sugupr1234@gmail.com>
2024-06-05 13:36:11 +0530

    add: variables.py file

---
+++ README.md
@@ -0,0 +1,9 @@
+## Python-Series
+
+This repository contains programming codes of `TechJackie Series`
+
+## Support
+
+If you have any doubts regarding python, we are there for you. Kindly, join our `TechJackie` Discord channel
+
+[TechJackie Discord Channel](https://discord.gg/95bRhheh)
---
+++ variables.py
@@ -0,0 +1,19 @@
+# variable_name = value
+
+glass_container_1 = "water"
+glass_container_2 = "oil"
+glass_container_3 = "sand"
+
+number = 1
+fraction_value = 1.2
+
+# String
+fruit_name = "Water Melon"
+
+# Boolean
+is_fruit = True
+
+# variable_name[start: stop: step]
+print(fruit_name[0:5:2]) # 0:4
+
+# 0 2 4
