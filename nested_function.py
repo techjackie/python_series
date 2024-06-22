1cce5e5d9b47d09819142d560c2e4080956a144a
TechJackie <161297231+techjackie@users.noreply.github.com>
2024-06-22 16:53:22 +0530

    add nested_function.py

---
+++ nested_function.py
@@ -0,0 +1,16 @@
+def get_cake():
+    print("*** Your order is preparing ***")
+
+    # nested function
+    def kitchen():
+        print("=> Baking the cake")
+        print("=> Cake is Ready")
+
+    kitchen()
+
+# function calling
+get_cake()
+
+
+   
+
